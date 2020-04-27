44a34b13768ebed86064ba0d92c4d565ef1c4ee5
ronn <ronnie@nextdigital.es>
2020-04-27 18:32:22 +0200

    Test Queue implementation

--- src/test/java/com/ronn/queue/model/QueueTest.java
+++ src/test/java/com/ronn/queue/model/QueueTest.java
@@ -1,4 +1,48 @@
 package com.ronn.queue.model;
 
+import com.ronn.queque.model.Queue;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class QueueTest {
+
+  private Queue q = new Queue();
+
+  @Test
+  public void canAdd(){
+    q = new Queue();
+    assertTrue(q.isEmpty());
+
+    q.enqueue("Hola");
+    assertFalse(q.isEmpty());
+  }
+
+  @Test
+  public void checkPeek(){
+    q = new Queue();
+    assertNull(q.peek());
+
+    q.enqueue("Adiós");
+    assertEquals(q.peek(), "Adiós");
+  }
+
+  @Test
+  public void canDequeue(){
+    q = new Queue();
+
+    q.enqueue("Hola");
+    assertFalse(q.isEmpty());
+
+    q.dequeue();
+    assertTrue(q.isEmpty());
+  }
+
+  @Test
+  public void canDispatch(){
+    String first = q.dispatch();
+
+    //assertFalse(q.peek(), );
+  }
+
 }
