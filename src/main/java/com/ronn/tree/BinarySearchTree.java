1bcb1f5183c52560c3189f4922c66d296164cc9e
ronn <ronnie@nextdigital.es>
2020-05-09 20:11:47 +0200

    BinarySearchTree partially implemented

--- src/main/java/com/ronn/tree/BinarySearchTree.java
+++ src/main/java/com/ronn/tree/BinarySearchTree.java
@@ -1,5 +1,91 @@
 package com.ronn.tree;
 
-public class BinarySearchTree {
+public class BinarySearchTree implements IBST<Integer> {
 
+  private Integer data;
+  private BinarySearchTree left, right;
+
+  @Override
+  public Boolean isLeaf() {
+    return data != null
+        && left == null
+        && right == null;
+  }
+
+  @Override
+  public Boolean isEmpty() {
+    return data == null;
+  }
+
+  @Override
+  public void insert(Integer newData) {
+    if (isEmpty()){
+      this.data = newData;
+    } else {//TODO check duplicate
+      if(newData.equals(this.data) ) throw new RuntimeException("Duplicados ni por el potas");
+
+      if (newData < data){
+        if (left == null){
+          left = new BinarySearchTree();
+        }
+        left.insert(newData);
+      } else {
+        if (right == null){
+          right = new BinarySearchTree();
+        }
+        right.insert(newData);
+      }
+    }
+  }
+
+  @Override
+  public Boolean exists(Integer id) {
+    if(isEmpty()) return false;
+
+    if (id.equals(data)) return true;
+
+    return id > data
+        ? right != null ? right.exists(id) : false
+        : left != null ? left.exists(id) : false;
+  }
+
+  @Override
+  public Integer get(Integer id) {
+    if(isEmpty()) return null;
+
+    if (id.equals(data)) return data;
+
+    return id > data
+        ? right != null ? right.get(id) : null
+        : left != null ? left.get(id) : null;
+  }
+
+  @Override
+  public void preorder() {
+
+  }
+
+  @Override
+  public void inorder() {
+
+  }
+
+  @Override
+  public void postorder() {
+
+  }
+
+  @Override
+  public void remove(Integer id) {
+
+  }
+
+
+  public BinarySearchTree getLeft() {
+    return left;
+  }
+
+  public BinarySearchTree getRight() {
+    return right;
+  }
 }
