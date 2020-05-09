0b43ce7fe3ac4a74f0e110597c1c736c3acae4c9
ronn <ronnie@nextdigital.es>
2020-05-09 20:12:19 +0200

    Test for progress in BST

--- src/test/java/com/ronn/tree/BSTTest.java
+++ src/test/java/com/ronn/tree/BSTTest.java
@@ -1,4 +1,69 @@
 package com.ronn.tree;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class BSTTest {
+
+  @Test
+  public void canInsert(){
+    BinarySearchTree bst = new BinarySearchTree();
+
+    assertTrue(bst.isEmpty());
+
+    bst.insert(2);
+    assertFalse(bst.isEmpty());
+    assertTrue(bst.isLeaf());
+
+    bst.insert(3);
+    assertFalse(bst.isLeaf());
+
+    assertFalse(bst.getRight().isEmpty());
+    assertTrue(bst.getRight().isLeaf());
+
+    bst.insert(1);
+
+    assertFalse(bst.getLeft().isEmpty());
+    assertTrue(bst.getLeft().isLeaf());
+  }
+
+  @Test
+  public void checkExists (){
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+    assertFalse(binarySearchTree.exists(2));
+
+    binarySearchTree.insert(2);
+
+    assertTrue(binarySearchTree.exists(2));
+
+    assertFalse(binarySearchTree.exists(3));
+    assertFalse(binarySearchTree.exists(1));
+
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(3);
+
+    assertTrue(binarySearchTree.exists(3));
+    assertTrue(binarySearchTree.exists(1));
+  }
+
+  @Test
+  public void canGetElementsFromTree (){
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
+
+    assertFalse(binarySearchTree.exists(2));
+
+    binarySearchTree.insert(2);
+
+    assertEquals(Integer.valueOf(2), binarySearchTree.get(2));
+    assertNull(binarySearchTree.get(1));
+    assertNull(binarySearchTree.get(5));
+
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(5);
+
+    assertNotNull(binarySearchTree.get(1));
+    assertNotNull(binarySearchTree.get(5));
+  }
 }
